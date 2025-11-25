ed8f7dfd2cdf4e38444f1e6f35822a3ced93724a
rogers0404 <rogers.ramirez2008@gmail.com>
2025-11-24 20:46:34 -0500

    commit these values

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        //print(sender.currentTitle!)
+        let hardness = sender.currentTitle
+        
+        /*if(hardness == "Soft"){
+            print(softTime)
+        }
+        else
+        if(hardness == "Medium"){
+            print(mediumTime)
+        }
+            else
+        if(hardness == "Hard"){
+                print(hardTime)
+        }*/
+        
+        switch hardness {
+        case "Soft":
+            print("Soft: \(softTime) min")
+        case "Medium":
+            print("Medium: \(mediumTime) min")
+        case "Hard":
+            print("Hard: \(hardTime) min")
+            
+        default:
+            print("--")
+        }
+    }
+    
 }
